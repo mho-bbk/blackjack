dbb6d0cf49debc23cc3a551cd34a95e30e0b1924
Kitty Mo Yin Ho <mho01@dcs.bbk.ac.uk>
2021-12-31 23:05:53 +0000

    Initial commit

---
+++ src/Blackjack.java
@@ -0,0 +1,10 @@
+public class Blackjack {
+
+    public Blackjack() {
+        //empty constructor
+    }
+
+    public static void play() {
+
+    }
+}
---
+++ src/BlackjackTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+public class BlackjackTest {
+
+    @org.junit.Test
+    public void play() {
+    }
+}
