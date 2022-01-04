b5b753b6a47651a661bc003c6229a14598af9dc4
Kitty Mo Yin Ho <mho01@dcs.bbk.ac.uk>
2022-01-04 21:44:06 +0000

    Reorganised files

--- src/test/BlackjackTest.java
+++ src/test/BlackjackTest.java
@@ -1,9 +1,5 @@
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.*;
 
 public class BlackjackTest {
 
@@ -14,46 +10,46 @@ public class BlackjackTest {
         int[] winningHand = new int[2];
         winningHand[0] = 21;
         winningHand[1] = 9;
-        assertEquals(0, game.evaluateHand(winningHand));
+        Assert.assertEquals(0, game.evaluateHand(winningHand));
 
         int[] secondWinningHand = new int[2];
         secondWinningHand[0] = 7;
         secondWinningHand[1] = 21;
-        assertEquals(0, game.evaluateHand(secondWinningHand));
+        Assert.assertEquals(0, game.evaluateHand(secondWinningHand));
 
         int[] bothBustHand = new int[2];
         bothBustHand[0] = 28;
         bothBustHand[1] = 23;
-        assertEquals(-1, game.evaluateHand(bothBustHand));
+        Assert.assertEquals(-1, game.evaluateHand(bothBustHand));
 
         int[] exampleStarterHand = new int[2];
         exampleStarterHand[0] = 9;   //hand[1] is 0 by default, no change
-        assertEquals(1, game.evaluateHand(exampleStarterHand));
+        Assert.assertEquals(1, game.evaluateHand(exampleStarterHand));
 
         int[] oneBustStillValidHand = new int[2];
         oneBustStillValidHand[0] = 23;  //bust
         oneBustStillValidHand[1] = 10;  //still valid
-        assertEquals(1, game.evaluateHand(oneBustStillValidHand));
+        Assert.assertEquals(1, game.evaluateHand(oneBustStillValidHand));
 
         int[] secondBustStillValidHand = new int[2];
         secondBustStillValidHand[0] = 10;  //still valid
         secondBustStillValidHand[1] = 23;  //bust
-        assertEquals(1, game.evaluateHand(secondBustStillValidHand));
+        Assert.assertEquals(1, game.evaluateHand(secondBustStillValidHand));
 
         int[] oneBustAndNotValid = new int[2];
         oneBustAndNotValid[0] = 23;  //this calc is bust
         //oneBustAndNotValid[1] is 0 by default. 0 score means there are no aces in the hand so the hand only has 1 possible score.
-        assertEquals(-1, game.evaluateHand(oneBustAndNotValid));
+        Assert.assertEquals(-1, game.evaluateHand(oneBustAndNotValid));
 
         int[] scenarioOneAndThree = new int[2];
         scenarioOneAndThree[0] = 11;
         scenarioOneAndThree[1] = 21;
-        assertEquals(0, game.evaluateHand(scenarioOneAndThree));
+        Assert.assertEquals(0, game.evaluateHand(scenarioOneAndThree));
 
         int[] scenarioTwo = new int[2];
         scenarioTwo[0] = 21;
         scenarioTwo[1] = 31;
-        assertEquals(0, game.evaluateHand(scenarioTwo));
+        Assert.assertEquals(0, game.evaluateHand(scenarioTwo));
     }
 
     public void testPlay() {
--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -1,8 +1,5 @@
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.*;
 
 public class HandTest {
